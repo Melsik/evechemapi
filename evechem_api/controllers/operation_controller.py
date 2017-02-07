cac837c3e2477108f59ea24c9daad6bbe9829be0
Myles Gallagher <myles.gallagher@gmail.com>
2017-02-06 22:50:39 -0500

    Operation Enpoints added & api_key parameters added to starbase endpoints.
    Operations added but not implemented.  API key authorization framework
    put in place across all secured enpoints, but not implemented yet.

---
+++ evechem_api/controllers/operation_controller.py
@@ -0,0 +1,132 @@
+import connexion
+from evechem_api.models.error import Error
+from evechem_api.models.operation import Operation
+from evechem_api.models.operation_name import OperationName
+from datetime import date, datetime
+from typing import List, Dict
+from six import iteritems
+from ..util import deserialize_date, deserialize_datetime
+
+
+def operation_delete(api_key):
+    """
+    operation_delete
+    Deletes the operation accessed by the given master key.  Note that this invalidates the key and deletes _all_ information under the operation including api keys, towers, and thier configurations. 
+    :param api_key: Operation Master Access Key
+    :type api_key: str
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def operation_get(api_key):
+    """
+    operation_get
+    Gets general information about the operation accessed by the given master key. 
+    :param api_key: Operation Master Access Key
+    :type api_key: str
+
+    :rtype: Operation
+    """
+    return 'do some magic!'
+
+
+def operation_keys_get(api_key):
+    """
+    operation_keys_get
+    Returns &#x60;array&#x60; of operation access sub-keys that exist for this operation. 
+    :param api_key: Operation Master Access Key
+    :type api_key: str
+
+    :rtype: List[str]
+    """
+    return 'do some magic!'
+
+
+def operation_keys_post(api_key):
+    """
+    operation_keys_post
+    Creates a new operation access sub-key with the access scopes provided, and then returns the key in the response. 
+    :param api_key: Operation Master Access Key
+    :type api_key: str
+
+    :rtype: str
+    """
+    return 'do some magic!'
+
+
+def operation_keys_sub_key_delete(sub_key, api_key):
+    """
+    operation_keys_sub_key_delete
+    Delete an access sub-key.  The specified key will no longer be valid for accessing any portion of the given operation. 
+    :param sub_key: Operation Access Sub-key
+    :type sub_key: str
+    :param api_key: Operation Master Access Key
+    :type api_key: str
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def operation_keys_sub_key_get(sub_key, api_key):
+    """
+    operation_keys_sub_key_get
+    Gets the permission settings of the given access key. 
+    :param sub_key: Operation Access Sub-key
+    :type sub_key: str
+    :param api_key: Operation Master Access Key
+    :type api_key: str
+
+    :rtype: KeyType
+    """
+    return 'do some magic!'
+
+
+def operation_keys_sub_key_patch(sub_key, api_key, key_type):
+    """
+    operation_keys_sub_key_patch
+    Update key_type for the given access sub-key. 
+    :param sub_key: Operation Access Sub-key
+    :type sub_key: str
+    :param api_key: Operation Master Access Key
+    :type api_key: str
+    :param key_type: 
+    :type key_type: dict | bytes
+
+    :rtype: KeyType
+    """
+    if connexion.request.is_json:
+        key_type = KeyType.from_dict(connexion.request.get_json())
+    return 'do some magic!'
+
+
+def operation_patch(api_key, operation_name):
+    """
+    operation_patch
+    Update either private or public name of the operation accessed with the provided master key. 
+    :param api_key: Operation Master Access Key
+    :type api_key: str
+    :param operation_name: Operation Name and Public Name
+    :type operation_name: dict | bytes
+
+    :rtype: Operation
+    """
+    if connexion.request.is_json:
+        operation_name = OperationName.from_dict(connexion.request.get_json())
+    return 'do some magic!'
+
+
+def operation_post(operation_name=None):
+    """
+    operation_post
+    Creates a new operation and returns operation details (including the &#x60;master key&#x60;). Optional parameters include the private name and public name of the operation.  These can be changed later. 
+    :param operation_name: Operation Name and Public Name
+    :type operation_name: dict | bytes
+
+    :rtype: Operation
+    """
+    if connexion.request.is_json:
+        operation_name = OperationName.from_dict(connexion.request.get_json())
+    return 'do some magic!'
--- evechem_api/controllers/starbase_controller.py
+++ evechem_api/controllers/starbase_controller.py
@@ -12,22 +12,26 @@ from six import iteritems
 from ..util import deserialize_date, deserialize_datetime
 
 
-def towers_get():
+def towers_get(api_key):
     """
     towers_get
     Gets summary information for every tower the request is authenticated to see.  This includes &#x60;tower_id&#x60; number, &#x60;type_id&#x60;, fuel status, as well as what the tower is producing at the endpoints of its reaction/mining chains. 
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: List[int]
     """
     return 'do some magic!'
 
 
-def towers_post(tower):
+def towers_post(tower, api_key):
     """
     towers_post
     Add new tower. 
     :param tower: New tower information in json format.
     :type tower: dict | bytes
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: None
     """
@@ -36,36 +40,42 @@ def towers_post(tower):
     return 'do some magic!'
 
 
-def towers_tower_id_delete(tower_id):
+def towers_tower_id_delete(tower_id, api_key):
     """
     towers_tower_id_delete
     Delete a specific tower and all its equipment/processes. 
     :param tower_id: Id number of the tower to be deleted.
     :type tower_id: int
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: None
     """
     return 'do some magic!'
 
 
-def towers_tower_id_get(tower_id):
+def towers_tower_id_get(tower_id, api_key):
     """
     towers_tower_id_get
     Gets details for a specific tower with &#x60;type_id&#x60;. 
     :param tower_id: Type of the item that needs fetched.
     :type tower_id: int
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: TowerDetails
     """
     return 'do some magic!'
 
 
-def towers_tower_id_patch(tower_id, tower_update=None):
+def towers_tower_id_patch(tower_id, api_key, tower_update=None):
     """
     towers_tower_id_patch
     
     :param tower_id: Id number of the tower to be deleted.
     :type tower_id: int
+    :param api_key: Operation Access Key
+    :type api_key: str
     :param tower_update: Details to update tower with.
     :type tower_update: dict | bytes
 
@@ -76,31 +86,35 @@ def towers_tower_id_patch(tower_id, tower_update=None):
     return 'do some magic!'
 
 
-def towers_tower_id_processes_get(tower_id):
+def towers_tower_id_processes_get(tower_id, api_key):
     """
     towers_tower_id_processes_get
     
     :param tower_id: Tower Id number to look under.
     :type tower_id: int
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: List[Process]
     """
     return 'do some magic!'
 
 
-def towers_tower_id_processes_post(tower_id):
+def towers_tower_id_processes_post(tower_id, api_key):
     """
     towers_tower_id_processes_post
     
     :param tower_id: Tower Id number to look under.
     :type tower_id: int
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: Process
     """
     return 'do some magic!'
 
 
-def towers_tower_id_processes_process_id_delete(tower_id, process_id):
+def towers_tower_id_processes_process_id_delete(tower_id, process_id, api_key):
     """
     towers_tower_id_processes_process_id_delete
     
@@ -108,13 +122,15 @@ def towers_tower_id_processes_process_id_delete(tower_id, process_id):
     :type tower_id: int
     :param process_id: Process Id number to look under.
     :type process_id: int
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: None
     """
     return 'do some magic!'
 
 
-def towers_tower_id_processes_process_id_equipment_equipment_id_delete(tower_id, process_id, equipment_id):
+def towers_tower_id_processes_process_id_equipment_equipment_id_delete(tower_id, process_id, equipment_id, api_key):
     """
     towers_tower_id_processes_process_id_equipment_equipment_id_delete
     
@@ -124,13 +140,15 @@ def towers_tower_id_processes_process_id_equipment_equipment_id_delete(tower_id,
     :type process_id: int
     :param equipment_id: Id of equipment that needs fetched.
     :type equipment_id: int
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: None
     """
     return 'do some magic!'
 
 
-def towers_tower_id_processes_process_id_equipment_equipment_id_get(tower_id, process_id, equipment_id):
+def towers_tower_id_processes_process_id_equipment_equipment_id_get(tower_id, process_id, equipment_id, api_key):
     """
     towers_tower_id_processes_process_id_equipment_equipment_id_get
     
@@ -140,13 +158,15 @@ def towers_tower_id_processes_process_id_equipment_equipment_id_get(tower_id, pr
     :type process_id: int
     :param equipment_id: Id of equipment that needs fetched.
     :type equipment_id: int
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: Equipment
     """
     return 'do some magic!'
 
 
-def towers_tower_id_processes_process_id_equipment_equipment_id_patch(tower_id, process_id, equipment_id, equipment):
+def towers_tower_id_processes_process_id_equipment_equipment_id_patch(tower_id, process_id, equipment_id, equipment, api_key):
     """
     towers_tower_id_processes_process_id_equipment_equipment_id_patch
     
@@ -158,6 +178,8 @@ def towers_tower_id_processes_process_id_equipment_equipment_id_patch(tower_id, 
     :type equipment_id: int
     :param equipment: 
     :type equipment: dict | bytes
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: Equipment
     """
@@ -166,7 +188,7 @@ def towers_tower_id_processes_process_id_equipment_equipment_id_patch(tower_id, 
     return 'do some magic!'
 
 
-def towers_tower_id_processes_process_id_equipment_get(tower_id, process_id):
+def towers_tower_id_processes_process_id_equipment_get(tower_id, process_id, api_key):
     """
     towers_tower_id_processes_process_id_equipment_get
     
@@ -174,13 +196,15 @@ def towers_tower_id_processes_process_id_equipment_get(tower_id, process_id):
     :type tower_id: int
     :param process_id: Process Id number to look under.
     :type process_id: int
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: List[Equipment]
     """
     return 'do some magic!'
 
 
-def towers_tower_id_processes_process_id_equipment_post(tower_id, process_id, new_equipment):
+def towers_tower_id_processes_process_id_equipment_post(tower_id, process_id, new_equipment, api_key):
     """
     towers_tower_id_processes_process_id_equipment_post
     
@@ -190,6 +214,8 @@ def towers_tower_id_processes_process_id_equipment_post(tower_id, process_id, ne
     :type process_id: int
     :param new_equipment: 
     :type new_equipment: dict | bytes
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: Equipment
     """
@@ -198,7 +224,7 @@ def towers_tower_id_processes_process_id_equipment_post(tower_id, process_id, ne
     return 'do some magic!'
 
 
-def towers_tower_id_processes_process_id_get(tower_id, process_id):
+def towers_tower_id_processes_process_id_get(tower_id, process_id, api_key):
     """
     towers_tower_id_processes_process_id_get
     
@@ -206,6 +232,8 @@ def towers_tower_id_processes_process_id_get(tower_id, process_id):
     :type tower_id: int
     :param process_id: Process Id number to look under.
     :type process_id: int
+    :param api_key: Operation Access Key
+    :type api_key: str
 
     :rtype: Process
     """
